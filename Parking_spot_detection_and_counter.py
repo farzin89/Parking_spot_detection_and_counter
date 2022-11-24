5bd176bc0d883bf1c48b48ba6b7529001711344d
farzin89 <farzin_ahmadi90@yahoo.com>
2022-11-24 15:39:17 +0100

    insert video and play

--- Parking_spot_detection_and_counter.py
+++ Parking_spot_detection_and_counter.py
@@ -1,1 +1,19 @@
-import 
+
+import cv2
+
+
+video_path = 'carPark.mp4'
+
+cap =cv2.VideoCapture(video_path)
+ret = True
+while True :
+    ret,frame = cap.read()
+
+
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+    
+cap.release()
+cv2.destroyAllWindows()
