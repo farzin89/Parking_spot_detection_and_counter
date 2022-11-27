8d8338a50d333b40e33c56d028fa47cda8694e58
farzin89 <farzin_ahmadi90@yahoo.com>
2022-11-27 10:35:17 +0100

    in order to detect real time

--- Parking_spot_detection_and_counter.py
+++ Parking_spot_detection_and_counter.py
@@ -11,18 +11,26 @@ cap =cv2.VideoCapture(video_path)
 connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
 spots =get_parking_spots_bboxes(connected_components)
 
+spots_status = [None for j in spots]
+frame_nmr = 0
 ret = True
-while True :
+step = 30
+while ret :
     ret,frame = cap.read()
 
     # plot how the bbox is look like
-    for spot in spots:
-        x1,y1,w,h = spot
-        # make a crop of frame
-        spot_crop = frame[y1:y1 +h, x1:x1 + w,:]
-        # is parking spot empty or not
-        spot_status =empty_or_not(spot_crop)
+    if frame_nmr % step == 0:
 
+         for spot_indx,spot in enumerate(spots):
+            x1,y1,w,h = spot
+            # make a crop of frame
+            spot_crop = frame[y1:y1 +h, x1:x1 + w,:]
+            # is parking spot empty or not
+            spot_status =empty_or_not(spot_crop)
+            spots_status[spot_indx] = spot_status
+    for spot_indx, spot in enumerate(spots):
+        spot_status =spots_status[spot_indx]
+        x1,y1,w,h = spots[spot_indx]
         if spot_status:
             frame=cv2.rectangle(frame,(x1,y1),(x1 + w,y1 + h),(0,255,0),2)
         else:
@@ -33,6 +41,6 @@ while True :
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
-
+    frame_nmr +=1
 cap.release()
 cv2.destroyAllWindows()
